ce0dab11ac839c3b4d1053945387e0263539c2a9
Wallace McGee <wm@wolispace.com>
2023-03-19 13:33:07 +1030

    random image transforms

--- utils.js
+++ utils.js
@@ -3,17 +3,39 @@ function rnd(max) {
 }
 
 function svgImg(svgName, svgClass = '', repeat = 1) {
-  let paths = ''
   let svgHtml = '';
-  svgClass = svgClass != '' ? svgClass : svgName;
   let svgInfo = svgImags[svgName];
+  let isSingle = repeat == 1;
+  let scale = 15;
   if (svgInfo) {
+
+    let paths = '';
+    svgClass = svgClass != '' ? svgClass : svgName;
     while (repeat > 0) {
+      let tx = 0;
+      let ty = 0;
+      let sx = 1;
+      let sy = 1;
+      let rotate = 0;
+      if (!isSingle) {
+
+        tx += rnd(scale) - (scale / 2);
+        ty += rnd(scale) - (scale / 2);
+        sx += (rnd(scale) - (scale / 2)) / 100;
+        sy += (rnd(scale) - (scale / 2)) / 100;
+        rotate = rnd(scale * 2) - (scale * 2 / 2);
+      }
+      paths += `<g transform="
+       translate(${tx},${ty}) 
+       scale(${sx} ${sy})
+       rotate(${rotate}) " >`;
+
       svgInfo.paths.forEach((path) => {
-        svgClass = path.c ? `${svgClass}-${path.c}` : svgClass;
-        paths += `<path class="${svgClass}" d="${path.d}" />`;
+        let svgCls = path.c ? `${svgClass}-${path.c}` : svgClass;
+        paths += `<path class="${svgCls}" d="${path.d}" />`;
       });
       repeat--;
+      paths += '</g>';
     }
 
     svgHtml = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
