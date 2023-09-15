a5a93ebb73cd1a836c593b05eda0dae454b167cb
Wallace McGee <wm@wolispace.com>
2023-09-15 17:38:35 +0930

    moving tool clicks

--- src2/Tool.js
+++ src2/Tool.js
@@ -4,7 +4,7 @@ class Tool extends game.Item {
     this.setup();
   }
 
-  setup () {
+  setup() {
     //this.sprite = this.getSprite();
     this.render();
 
@@ -13,38 +13,18 @@ class Tool extends game.Item {
     });
 
     return;
-    
-    let idx = `#i${this.id}`;
-    let pick = document.querySelector("#ipick");
-    console.log('pick', pick);
-    this.thing.addEventListener("click", function(){
-      console.log('clicked');
-      tools.list['pick'].onClick;
+
+    this.sprite.addEventListener("keydown", (event) => {
+      console.trace('setup?');
+      //event.preventDefault();
+      this.onClick();
     });
-    console.log('tool setop', this, idx);
-    
 
-    let id = `#i${this.id}`;
-    this.sprite = document.querySelector(id);
-                  
-    this.onClick = function () {alert('hi')};
-
-
-    console.trace('setup', this);
-    if (true) {
-      this.sprite.addEventListener("keydown", (event) => {
-        console.trace('setup?');
-        //event.preventDefault();
-        this.onClick();
-      });
-  
-      this.sprite.addEventListener("keyup", (event) => {
-        //event.preventDefault();
-        this.onClickEnd();
-      });
-    }
+    this.sprite.addEventListener("keyup", (event) => {
+      //event.preventDefault();
+      this.onClickEnd();
+    });
   }
-
 
   onClick = function () {
     console.log('default onclick');
--- src2/tools.js
+++ src2/tools.js
@@ -10,11 +10,62 @@ const tools = {
 
     toolList.forEach((itemName) => {
       tools.list[itemName] = new Tool(itemName, sprite.width * leftX , sprite.height * leftY);
-      tools.list[itemName].onClick = function () {
-        console.log('Clicked ', this.id);
-      }
+      tools.list[itemName].onClick = tools.clicks[itemName];
       leftX = leftX + 2;
     });
   },
+
+  clicks: {
+    pick: function () {
+      let title = "Pick";
+      let content = `<div class="dialog-message-content">`;
+      content += `This is your pick.`;
+      let footer = "";
+      footer += `<button class="buttonize" onclick="dialog.confirm()"> Ok </button>`;
+      dialog.cancelButton = function () { dialog.hide(); };
+      dialog.okButton = function () { dialog.hide(); };
+      dialog.render(title, content, footer);
+    },
+    spade: function () {
+      let title = "Spade";
+      let content = `<div class="dialog-message-content">`;
+      content += `This is your spade.`;
+      let footer = "";
+      footer += `<button class="buttonize" onclick="dialog.confirm()"> Ok </button>`;
+      dialog.cancelButton = function () { dialog.hide(); };
+      dialog.okButton = function () { dialog.hide(); };
+      dialog.render(title, content, footer);
+    },
+    axe: function () {
+      let title = "axe";
+      let content = `<div class="dialog-message-content">`;
+      content += `This is your axe.`;
+      let footer = "";
+      footer += `<button class="buttonize" onclick="dialog.confirm()"> Ok </button>`;
+      dialog.cancelButton = function () { dialog.hide(); };
+      dialog.okButton = function () { dialog.hide(); };
+      dialog.render(title, content, footer);
+    },
+    scanner: function () {
+      let title = "Scanner";
+      let content = `<div class="dialog-message-content">`;
+      content += `This is your scanner.`;
+      let footer = "";
+      footer += `<button class="buttonize" onclick="dialog.confirm()"> Ok </button>`;
+      dialog.cancelButton = function () { dialog.hide(); };
+      dialog.okButton = function () { dialog.hide(); };
+      dialog.render(title, content, footer);
+    },
+    wallet: function () {
+      let title = "Wallet";
+      let content = `<div class="dialog-message-content">`;
+      content += `This is your wallet.`;
+      let footer = "";
+      footer += `<button class="buttonize" onclick="dialog.confirm()"> Ok </button>`;
+      dialog.cancelButton = function () { dialog.hide(); };
+      dialog.okButton = function () { dialog.hide(); };
+      dialog.render(title, content, footer);
+    },
+  }
 };
 
