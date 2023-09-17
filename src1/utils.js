2ef374beca941190a57d5fcc7cb8589af11295df
Wallace McGee <wm@wolispace.com>
2023-09-17 14:40:28 +0930

    removed old function

--- src1/utils.js
+++ src1/utils.js
@@ -53,17 +53,6 @@ const state = {
   }
 };
 
-function html(selector, text) {
-  let elem = checkSelector(selector);
-
-  if (text) {
-    elem.innerHTML = text;
-  } else {
-    return elem.innerHTML;
-  }
-  return elem;
-}
-
 function checkSelector(selector) {
   let elem = selector;
   if (typeof selector === "string") {
