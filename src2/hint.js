ed8491c267f83830ad70d57c48b9f488321b1f55
Wallace McGee <wm@wolispace.com>
2023-10-27 13:30:23 +1030

    pointing hint nicely

--- src2/hint.js
+++ src2/hint.js
@@ -6,7 +6,7 @@ const hint = {
   okButton: '',
   group: '',
   visible: false,
-  
+
   defaultParams: {
     autoRender: false,
   },
@@ -14,27 +14,27 @@ const hint = {
   setup: function () {
     // make two objects.. the body and the arrow
     let hintBox = document.querySelector(`.hintArrow`);
+    let arrowSvg = svg.render('arrow', 1, '');
 
     if (!hintBox) {
       addToBody(`
-        <div class='hintArrow'></div>
+        <div class='hintArrow'>${arrowSvg}</div>
         <div class='hintMsg'></div>
       `);
     }
     hint.arrow = document.querySelector(`.hintArrow`);
     hint.msg = document.querySelector(`.hintMsg`);
+
     hint.hide();
   },
 
   test: function () {
     dialog.hide();
-    hint.target = buildings.list.home;
-    hint.target = buildings.list.cart;
-    //hint.target = tools.list.scanner;
-    hint.render();
+    hint.render(game.playerItem);
   },
 
-  render: function () {
+  render: function (item) {
+    hint.target = item;
     if (player.hinted[hint.group] || !player.hints) {
       hint.hide();
       return;
@@ -44,83 +44,84 @@ const hint = {
   },
 
   pointAt: function () {
-    let orient = hint.orient();
+    hint.setOrient();
     let tCtr = hint.target.centre();
-    console.log('orient', orient);
     let newPos = {
       x: tCtr.x,
       y: tCtr.y,
     }
-    if (orient.x < 1) {
-      newPos.x = newPos.x + hint.arrow.w;
+    if (hint.orient.x == -1) {
+      newPos.x = newPos.x - hint.arrow.offsetWidth;
     }
-    if (orient.y > 1) {
-      newPos.y = newPos.y - hint.arrow.h;
+    if (hint.orient.y == -1) {
+      newPos.y = newPos.y - hint.arrow.offsetWidth;
     }
-    console.log(`newPos`, newPos);
     hint.arrow.style.left = `${newPos.x}px`;
     hint.arrow.style.top = `${newPos.y}px`;
-
+    // flip the image to point at a corner.
+    hint.arrow.style.transform = `scale(${hint.orient.x}, ${hint.orient.y})`;
   },
-  
-  // is the target < or > the centre of the world
-  orient: function () {
-    let worldCentre = {
-      x: sprite.width * game.grid.x / 2,
-      y: sprite.height * game.grid.y / 2,
-    }
-    console.log('ctr', worldCentre, hint.target.centre());
 
-    return {
-      x: (hint.target.centre.x < worldCentre.x) ? -1 : 1,
-      y: (hint.target.centre.y < worldCentre.y) ? -1 : 1,
+  // is the target < or > the centre of the world
+  setOrient: function () {
+    let worldCentre = {
+      x: (sprite.width * game.grid.x) / 2,
+      y: (sprite.height * game.grid.y) / 2,
+    }
+    let tCtr = hint.target.centre();
+
+    hint.orient = {
+      x: (tCtr.x < worldCentre.x) ? 1 : -1,
+      y: (tCtr.y < worldCentre.y) ? 1 : -1,
     }
   },
 
   hide: function () {
-    hint.msg.style.display = 'none';
-    hint.arrow.style.display = 'none';
+    hint.msg.style.top = '-1000px';
+    hint.arrow.style.top = '-1000px';
   },
 
   show: function () {
-    hint.msg.style.display = 'block';
-    hint.arrow.style.display = 'block';
-  },
+    // align msg with arrow that is already pointing at the item
+    hint.msg.style.top = ``
+    if (hint.orient.x == -1) {
+    };
+    },
 
-  // a random OK message
-  ok: function () {
-    okText = [
-      "Ok",
-      "Okay",
-      "Righto",
-      "Gotcha",
-      "I see",
-      "Interesting",
-      "Thanks",
-      "Got that",
-      "Roger",
-      "Understood",
-      "Aha",
-      "Go",
-      "Yup",
-      "Makes sense",
-      "Agreed",
-      "Affirmative",
-    ];
-    return okText[rnd(okText.length)];
-  },
+    // a random OK message
+    ok: function () {
+      okText = [
+        "Ok",
+        "Okay",
+        "Righto",
+        "Gotcha",
+        "I see",
+        "Interesting",
+        "Thanks",
+        "Got that",
+        "Roger",
+        "Understood",
+        "Aha",
+        "Go",
+        "Yup",
+        "Makes sense",
+        "Agreed",
+        "Affirmative",
+      ];
+      return okText[rnd(okText.length)];
+    },
 
-  random: function () {
-    let randomHint = [
-      "You may find things other than potatoes buried beneath you",
-      "There are 4 levels of scanner upgrade. The 4th shows what's directly under you",
-      "Everything is saved, all the time (in your browser's local storage)",
-      "If you have a keyboard, use cursor, space, enter and escape keys",
-      "It takes 30 random white circles to make one cloud",
-      "The rarest potatoes are found on the furthest fields",
-    ];
+    random: function () {
+      let randomHint = [
+        "You may find things other than potatoes buried beneath you",
+        "There are 4 levels of scanner upgrade. The 4th shows what's directly under you",
+        "Everything is saved, all the time (in your browser's local storage)",
+        "If you have a keyboard, use cursor, space, enter and escape keys",
+        "It takes 30 random white circles to make one cloud",
+        "The rarest potatoes are found on the furthest fields",
+      ];
 
-    return randomHint[rnd(randomHint.length)];
-  },
+      return randomHint[rnd(randomHint.length)];
+    },
 
-};
+  };
