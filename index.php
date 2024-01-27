6bd7f158a351b45e452c741e55d6eaf1937f576a
Wallace McGee <wm@wolispace.com>
2024-01-28 09:20:06 +1030

    moved icons into subfolder

--- index.php
+++ index.php
@@ -114,10 +114,10 @@ function outputGamePage($v, $id = '')
     <meta charset='utf-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0, interactive-widget=resizes-visual' />
     <link rel='manifest' href='manifest.json'>
-    <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png'>
-    <link rel='icon' type='image/png' sizes='32x32' href='favicon-32x32.png'>
-    <link rel='icon' type='image/png' sizes='16x16' href='favicon-16x16.png'>
-    <link rel='mask-icon' href='safari-pinned-tab.svg' color='#4b965f'>
+    <link rel='apple-touch-icon' sizes='180x180' href='icons/apple-touch-icon.png'>
+    <link rel='icon' type='image/png' sizes='32x32' href='icons/favicon-32x32.png'>
+    <link rel='icon' type='image/png' sizes='16x16' href='icons/favicon-16x16.png'>
+    <link rel='mask-icon' href='icons/safari-pinned-tab.svg' color='#4b965f'>
     <meta name='msapplication-TileColor' content='#8ec39c'>
     <meta name='theme-color' content='#ffffff'>
     <link rel='stylesheet' href='layout.css?{$v}' />
